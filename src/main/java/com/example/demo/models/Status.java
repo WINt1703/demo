fc3fe1c6c4b47e1ba2ec50e199533eebe369bc56
Your Name <isuszanty@gmail.com>
2025-12-13 22:50:19 +0100

    change Status from entity to enum

--- src/main/java/com/example/demo/models/Status.java
+++ src/main/java/com/example/demo/models/Status.java
@@ -1,41 +1,7 @@
 package com.example.demo.models;
 
-import jakarta.persistence.*;
-
-import java.util.Set;
-
-@Entity
-@Table(name = "status")
-public class Status {
-    @Id
-    private long id;
-
-    private String title;
-
-    @OneToMany(mappedBy = "status")
-    private Set<Order> orders;
-
-    public long getId() {
-        return this.id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Set<Order> getOrders() {
-        return this.orders;
-    }
-
-    public void setOrders(Set<Order> orders) {
-        this.orders = orders;
-    }
+public enum Status {
+    NEW,
+    CANCELED,
+    COMPLETED,
 }
