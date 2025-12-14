c4417b8d3a435165aa23e1c05084d49a277c6e4d
Your Name <isuszanty@gmail.com>
2025-12-14 20:51:59 +0100

    refactor deleteProduct in ProductService

--- src/main/java/com/example/demo/services/ProductService.java
+++ src/main/java/com/example/demo/services/ProductService.java
@@ -3,11 +3,16 @@ package com.example.demo.services;
 import com.example.demo.dtos.Product.ResponseProductDto;
 import com.example.demo.dtos.Product.RequestProductDto;
 import com.example.demo.mappers.ProductMapper;
+import com.example.demo.models.Order;
+import com.example.demo.models.OrderProduct;
 import com.example.demo.models.Product;
 import com.example.demo.repositories.*;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
@@ -17,18 +22,21 @@ public class ProductService {
     private final CategoryRepository categoryRepository;
     private final ManufacturerRepository manufacturerRepository;
     private final ProductMapper productMapper;
+    private final OrderProductRepository orderProductRepository;
 
     public ProductService(ProductRepository productRepository,
                            SupplierRepository supplierRepository,
                            UnittypeRepository unittypeRepository,
                            CategoryRepository categoryRepository,
                            ManufacturerRepository manufacturerRepository,
+                           OrderProductRepository orderProductRepository,
                            ProductMapper productMapper) {
         this.productRepository = productRepository;
         this.supplierRepository = supplierRepository;
         this.categoryRepository = categoryRepository;
         this.unittypeRepository = unittypeRepository;
         this.manufacturerRepository = manufacturerRepository;
+        this.orderProductRepository = orderProductRepository;
         this.productMapper = productMapper;
     }
 
@@ -70,10 +78,15 @@ public class ProductService {
         return Optional.empty();
     }
 
+    @Transactional
     public boolean deleteProduct(String productId) {
         try {
-            if (productRepository.existsById(productId)) {
-                productRepository.deleteById(productId);
+            var product = this.productRepository.findById(productId);
+
+            if (product.isPresent()) {
+                var unwrappedProduct = product.orElseThrow();
+                this.orderProductRepository.deleteAll(unwrappedProduct.getOrderProducts());
+                this.productRepository.delete(unwrappedProduct);
                 return true;
             }
         }
