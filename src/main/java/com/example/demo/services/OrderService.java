f84bb56eeaf5dee798670b688cb06d9acae58276
Your Name <isuszanty@gmail.com>
2025-12-14 20:51:45 +0100

    refactor createOrder in OrderService

--- src/main/java/com/example/demo/services/OrderService.java
+++ src/main/java/com/example/demo/services/OrderService.java
@@ -10,12 +10,12 @@ import com.example.demo.repositories.OrderProductRepository;
 import com.example.demo.repositories.OrderRepository;
 import com.example.demo.repositories.PickUpPointRepository;
 import com.example.demo.repositories.ProductRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
@@ -23,30 +23,41 @@ public class OrderService {
     private final ProductRepository productRepository;
     private final PickUpPointRepository pickUpPointRepository;
     private final OrderMapper orderMapper;
+    private final OrderProductRepository orderProductRepository;
 
     public OrderService(OrderRepository orderRepository,
                         OrderMapper orderMapper,
                         PickUpPointRepository pickUpPointRepository,
+                        OrderProductRepository orderProductRepository,
                         ProductRepository productRepository) {
         this.orderRepository = orderRepository;
         this.orderMapper = orderMapper;
         this.pickUpPointRepository = pickUpPointRepository;
         this.productRepository = productRepository;
+        this.orderProductRepository = orderProductRepository;
     }
 
+    @Transactional
     public Optional<ResponseOrderDto> createOrder(RequestOrderDto orderDto, String username) {
         try {
             var pickUp = this.pickUpPointRepository.findById(orderDto.pickUpPointId()).orElseThrow();
             var order = new Order();
+            order.setOrderProducts(new HashSet<>());
 
             this.orderMapper.copyToEntity(order, orderDto, pickUp, username);
 
             for(ProductIdCount productIdCount: orderDto.productIdCountCollection()) {
                 var product = this.productRepository.findById(productIdCount.productId()).orElseThrow();
                 var orderProduct = new OrderProduct();
-                product.addOrderProduct(orderProduct);
-                order.addOrderProduct(orderProduct);
+
+                product.getOrderProducts().add(orderProduct);
+                order.getOrderProducts().add(orderProduct);
+
+                orderProduct.setProduct(product);
+                orderProduct.setOrder(order);
                 orderProduct.plusCount(productIdCount.count());
+
+                this.orderProductRepository.save(orderProduct);
             }
 
             return Optional.of(this.orderMapper.fromEntity(this.orderRepository.save(order)));
